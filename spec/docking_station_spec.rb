4f9d9e4837f0895adb29b56f7d0d8b62f2a770c5
etelish <etelisha@googlemail.com>
2019-08-23 14:58:42 +0100

    ex 10 completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,11 @@ describe DockingStation do
   it "class docking station responds to release bike method" do
   expect(subject).to respond_to(:release_bike)
   end
+it "releases a bike " do
+  expect(subject.release_bike).to be_an_instance_of(Bike)
+end
+
+it "expects bike to be working" do
+expect(subject.release_bike.working?).to eq true
+end
 end
